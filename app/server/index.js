d3d5aecafcab6767c2508e07349b05c70a8268cc
Nils <87240201+Sliner05@users.noreply.github.com>
2023-03-15 09:36:22 +0100

    Structure of Project + imports
    Project + axios, cors, sha265, react, mysql

---
+++ app/server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+const { response } = require("express");
+const sha256 = require("js-sha256");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "",
+  host: "i-kf.ch",
+  password: "",
+  database: "",
+});
+db.connect((err) => {
+  if (err) {
+    console.log("DB Connection error: ", err);
+  } else {
+    console.log("Connected to DB!");
+  }
+});
+
+// TODO Code Logic
+
+app.listen(3030, () => {
+  console.log("da Boi running on port  3001");
+});

e1b05f18418f0fbc9822e18b179b0444410ffd76
Nils <87240201+Sliner05@users.noreply.github.com>
2023-04-19 10:17:11 +0200

    Datenbank Aufsetzen : 3 : Datenbank tables und script erstellt

--- DB/database.sql
+++ DB/database.sql
@@ -1,34 +1,47 @@
 
-USE `GreenCamp` ;
+-- -----------------------------------------------------
+-- Schema SC_CampoDB
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS SC_CampoDB ;
 
 -- -----------------------------------------------------
--- Table `GreenCamp`.`Tareas`
+-- Schema SC_CampoDB
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `GreenCamp`.`Tareas` (
-  `areaId` VARCHAR(6) NOT NULL,
-  PRIMARY KEY (`areaId`))
+CREATE SCHEMA IF NOT EXISTS SC_CampoDB DEFAULT CHARACTER SET utf8mb4 ;
+USE SC_CampoDB ;
+
+-- -----------------------------------------------------
+-- Table TAreas
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS TAreas (
+  AreaId VARCHAR(6) NOT NULL,
+  PRIMARY KEY (AreaId))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `GreenCamp`.`Tcamper`
+-- Table TCamper
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `GreenCamp`.`Tcamper` (
-  `camperId` INT NOT NULL AUTO_INCREMENT,
-  `camperVorname` VARCHAR(45) NULL,
-  `CamperNachname` VARCHAR(45) NULL,
-  `camperFzNr` VARCHAR(15) NULL,
-  PRIMARY KEY (`camperId`))
+CREATE TABLE IF NOT EXISTS TCamper (
+  CamperId INT NOT NULL AUTO_INCREMENT,
+  CamperVorname VARCHAR(45) NULL,
+  CamperNachname VARCHAR(45) NULL,
+  CamperFzNr VARCHAR(15) NULL,
+  CamperStrNr CHAR(5) NULL,
+  CamperStrasse VARCHAR(30) NULL,
+  CamperOrt VARCHAR(30) NULL,
+  CamperLand VARCHAR(30) NULL,
+  CamperKredNr CHAR(16) NULL,
+  PRIMARY KEY (CamperId))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `GreenCamp`.`Treservationen`
+-- Table TReservationen
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `GreenCamp`.`Treservationen` (
-  `reservationId` VARCHAR(45) NOT NULL,
-  `Tareas.areaId` VARCHAR(6) NOT NULL,
-  `Tareas_areaId` VARCHAR(6) NOT NULL,
-  `Tcamper_camperId` INT NOT NULL,
-  PRIMARY KEY (`reservationId`))
+CREATE TABLE IF NOT EXISTS TReservationen (
+  ResId VARCHAR(45) NOT NULL,
+  AreaId VARCHAR(6) NOT NULL,
+  CamperId INT NOT NULL,
+  PRIMARY KEY (ResId))
 ENGINE = InnoDB;

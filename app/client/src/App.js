4d64fba7e4e43f4ae59a2ab63f956b6d56a86b8e
Nils <87240201+Sliner05@users.noreply.github.com>
2023-05-04 09:21:03 +0200

    Administrative : 2: branches aufgeräumt

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Map from "./components/map/Map.js";
 import Navbar from "./components/navbar/Navbar.jsx";
 import { useState } from "react";
@@ -10,25 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>we
-      </header>
-      <body>
-        <h1>
-          title
-        </h1>
-      </body>
+      <Map></Map>
     </div>
   );
 }
